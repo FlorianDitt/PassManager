1a3b7bc9f55516b7b0a909649469d21fbab82e8b
FlorianDitt <118379287+FlorianDitt@users.noreply.github.com>
2023-01-20 19:13:20 +0100

    added delete and edit button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,32 +1,39 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
-import android.widget.Button
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
     private var userID : String = ""//Get user id from LoginActivity
+    private var username : String = ""//Get user id from LoginActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (intent.extras != null){
             userID = intent.getStringExtra("UserID").toString()
+            username = intent.getStringExtra("Username").toString()
         }
+        findViewById<TextView>(R.id.displayUsername).text = username
 
-        val toAddButton : Button = findViewById(R.id.toAddButton)
+        val toAddButton : ImageButton = findViewById(R.id.toAddButton)
+        val delButton: ImageButton = findViewById(R.id.deleteButton)
+        val showDel : Boolean = false
 
         toAddButton.setOnClickListener {
             val intent = Intent(this,AddPasswordActivity::class.java)//Declare target Activity
             intent.putExtra("UserID", userID)//add userID to new Activity
             startActivity(intent)//change to Activity to add Password
+        }
+
+        delButton.setOnClickListener {
+
         }
 
         createTable()
@@ -47,7 +54,7 @@ class MainActivity : AppCompatActivity() {
 
             val tv1 = TextView(this)
             tv1.text = website
-            tv1.gravity = Gravity.CENTER_HORIZONTAL
+            tv1.gravity = Gravity.CENTER_VERTICAL
             tv1.setPadding(15,15,15,15)
             tblRow.addView(tv1)
 
@@ -62,6 +69,12 @@ class MainActivity : AppCompatActivity() {
             tv3.gravity = Gravity.CENTER_HORIZONTAL
             tv3.setPadding(15,15,15,15)
             tblRow.addView(tv3)
+
+            val ib = ImageButton(this)
+            ib.setImageResource(R.drawable.delete)
+            ib.setBackgroundColor(Color.TRANSPARENT)
+            ib.scaleType = ImageView.ScaleType.FIT_CENTER
+            tblRow.addView(ib)
 
             tableLayout.addView(tblRow)
 
